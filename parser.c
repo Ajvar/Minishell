a1627a6f3943cd7b6030be8bb95b7ef4926dd788
Ajvar <cueille@protonmail.com>
2021-03-08 19:56:52 +0100

    Working on backslashes

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 17:12:48 by jcueille          #+#    #+#             */
-/*   Updated: 2021/03/05 13:31:37 by jcueille         ###   ########.fr       */
+/*   Updated: 2021/03/08 19:56:03 by jcueille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,50 @@ typedef struct		s_env
 
 t_env *g_env;
 
-void ft_new_env()
+void	ft_new_env(void)
 {
+	t_env *tmp;
+
 	g_env = malloc(sizeof(t_env));
 	g_env->key = ft_strdup("VAR");
 	g_env->value = ft_strdup("VALUE");
-	t_env *tmp;
 	tmp = malloc(sizeof(t_env));
 	g_env->next = tmp;
 	tmp->key = ft_strdup("VARZ");
 	tmp->value = ft_strdup("VALUZ");
 }
 
-//REMOVE A SINGLE CHAR FROM A STRING
+/*
+**	REMOVE A SINGLE CHAR FROM A STRING
+*/
+
 static void	ft_remove_char(char *s, int i)
 {
 	char	*res;
 	int		j;
 
-	if (!(res = malloc(ft_strlen(s)  * sizeof(char))))
+	if (!(res = malloc(ft_strlen(s) * sizeof(char))))
 		return ;
 	j = -1;
-	while(++j < i)
+	while (++j < i)
 		res[j] = s[j];
 	i = j;
-	while(s[++j])
+	while (s[++j])
 	{
 		res[i] = s[j];
 		i++;
 	}
-	res[j - 1] = '\0'; 
+	res[j - 1] = '\0';
 	free(s);
 	s = ft_strdup(res);
 	free(res);
 }
 
-//CHECKS IF VAR BRACKETS ARE CLOSED
-static int ft_bracket_check(char *s, int i)
+/*
+**	CHECKS IF VAR BRACKETS ARE CLOSED
+*/
+
+static int	ft_bracket_check(char *s, int i)
 {
 	while (s[i])
 	{
@@ -71,54 +78,48 @@ static int ft_bracket_check(char *s, int i)
 	return (0);
 }
 
-static void ft_error()
+static void	ft_error(void)
 {
 	printf("error\n");
 	exit(1);
 }
 
-static void ft_expand(char *s, int i)
+static void	ft_expand(char *s, int i)
 {
 	char	*res;
-
 
 	res = NULL;
 	if (s[i + 1] == '{' || ft_isalpha(s[i + 1]))
 	{
 		ft_remove_char(s, i);
 		if (s[i] == '{')
-		{	
+		{
 			if (!(ft_bracket_check(s, i)))
 				ft_error();
 		}
-	
 	}
-
 }
 
+/*
+**	STRING SANS QUOTES
+*/
 
-
-
-
-
-
-// STRING SANS QUOTES
 static char	*ft_string(char *s, int *i)
 {
 	int		j;
-	//t_list	*tmp;
 	char	*res;
 
 	j = *i;
-	while(s[j] && s[j] != ' ' && s[j] != '\"' && s[j] != '\'' && s[j] != '$')
+	while (s[j] && s[j] != ' ' && s[j] != '\"' && s[j] != '\'' && s[j] != '$')
 	{
 		if (s[j] == '\\')
+		{
 			ft_remove_char(s, j);
+			j++;
+		}
 		j++;
 	}
 	res = ft_substr(s, *i, j - *i);
-	//tmp = ft_lstnew(res);
-	//ft_lstadd_back(&command, tmp);
 	(*i) = j - 1;
 	return (res);
 }
@@ -126,64 +127,61 @@ static char	*ft_string(char *s, int *i)
 static char	*ft_single(char *s, int *i)
 {
 	int		j;
-	//t_list	*tmp;
 	char	*res;
 
 	j = (*i) + 1;
-	while(s[j] && s[j] != '\'')
+	while (s[j] && s[j] != '\'')
 	{
 		if (s[j] == '\\')
 			ft_remove_char(s, j);
 		j++;
 	}
 	res = ft_substr(s, (*i) + 1, j - (*i + 1));
-	//ft_lstadd_back(&command, tmp);
 	(*i) = j;
 	return (res);
 }
 
-//RETRIEVE VAR VALUE
+/*
+**	RETRIEVE VAR VALUE
+*/
+
 static char	*ft_search_var(char *s, int *inc, int flag, int *i)
 {
-	//int		i;
 	int		k;
 	int		j;
 	t_env	*tmp;
 	char	*name;
 	char	*res;
 
-	//i = 1;
-	k = *i;
-	
+	k = *i + 1;
 	(*inc) = 0;
 	tmp = g_env;
 	res = NULL;
-	if (!(s[i]))
+	if (!(s[k]))
 		return (res);
-	if (s[i] == '{')
+	if (s[k] == '{')
 	{
-		if (!(ft_bracket_check(s, i)))
+		if (!(ft_bracket_check(s, k)))
 			return (res);
 		(*inc) += 2;
-		i++;
+		k++;
 	}
-	j = i;
+	j = k;
 	while (s[j] && s[j] != ' ' && s[j] != '}')
 		j++;
-		name = ft_substr(s, i, j - i);
+	name = ft_substr(s, k, j - k);
 	while (tmp)
 	{
-		//if (ft_strncmp(tmp->key, name, ft_strlen(tmp->key)) == 0)
-		//	res = ft_strdup(tmp->value);
-		int l = ft_strncmp(tmp->key, name, ft_strlen(tmp->key));
-		if (l == 0)
+		if (ft_strncmp(tmp->key, name, ft_strlen(tmp->key)) == 0)
 			res = ft_strdup(tmp->value);
+		/*int l = ft_strncmp(tmp->key, name, ft_strlen(tmp->key));
+		**	if (l == 0)
+		**	res = ft_strdup(tmp->value);
+		*/
 		tmp = tmp->next;
 	}
 	*inc += ft_strlen(name);
 	free(name);
-	if (s[1] != '{')
-		(*inc) -= 2;
 	if (s[++j] && flag == 1)
 		name = ft_strjoin(res, &s[j]);
 	else
@@ -192,62 +190,64 @@ static char	*ft_search_var(char *s, int *inc, int flag, int *i)
 	return (name);
 }
 
+/*
+**	DOLLAR DANS QUOTES
+*/
 
-//DOLLAR DANS QUOTES
 static char	*ft_dollar(char *s, int *i, int *j)
 {
 	char	*tmp;
-	char	*var;
+	char	*tmp2;
+	char	*res;
 	int		inc;
 
-	var = ft_search_var(&s[*j], &inc, 1);
-	s[*j] = '\0';
-	*j = inc;
-	tmp = ft_strjoin(s, var);
-	//free(s);
-	//s = ft_strdup(tmp);
-	return (tmp);
+	res = ft_search_var(s, &inc, 1, j);
+	*j = inc - 1;
+	free(s);
+	return (res);
 }
 
+/*
+**	DOUBLE QUOTES
+*/
 
-// DOUBLE QUOTES
-static char *ft_double(char *s, int *i)
+static char	*ft_double(char *s, int *i)
 {
-	int j;
-	char *res;
+	int		j;
+	char	*s_copy;
+	char	*res;
 
-	//ft_remove_char(s, *i);
-	(*i)++;
-	j = *i;
-	if (!(s[*i]))
-		return NULL;
-	while (s[j] && s[j] != '\"')
+	j = *i + 1;
+	if (!(s[j]))
+		return (NULL);
+	s_copy = ft_strdup(&s[j]);
+	j = 0;
+	while (s_copy[j] && s_copy[j] != '\"')
 	{
-		if (s[j] == '\\')
-		{	
-			if (s[j + 1] == '\\' || s[j + 1] == '\"')
+		if (s_copy[j] == '\\')
+		{
+			if (s_copy[j + 1] == '\\' || s_copy[j + 1] == '\"')
 			{
-				ft_remove_char(s, j);
+				ft_remove_char(s_copy, j);
 				j++;
 			}
 		}
 		if (s[j] == '$')
-			s = ft_dollar(s, i, &j);
+			s_copy = ft_dollar(s_copy, i, &j);
 		j++;
 	}
-	if (s[j] == '\0')
-		printf("ftprompt");
-	//²else
-	//ft_remove_char(s, j);	
-		//remove " et check si on a une string après, si on a space ou \0 on arrete sinon on append.
-	
-	res = ft_substr(s, *i, j - (*i + 1));
-	(*i) = j + 1;
-	return (res);
+	if (s_copy[j] == '\0')
+		printf("ERROR: DOUBLE QUOTE NOT CLSOED\n");
+	s_copy[j] = '\0';
+	(*i) += j + 1;
+	return (s_copy);
 }
 
-//APPLY FT ACCORDING TO QUOTES OR NOT
-char	*ft_apply_ft(char *s, int *i, char *(*f)(char *s, int *i), char *res)
+/*
+**	APPLY FT ACCORDING TO QUOTES OR NOT
+*/
+
+char		*ft_apply(char *s, int *i, char *(*f)(char *s, int *i), char *res)
 {
 	char	*tmp;
 	char	*tmp_bis;
@@ -265,7 +265,8 @@ char	*ft_apply_ft(char *s, int *i, char *(*f)(char *s, int *i), char *res)
 
 
 //ADDS STR TO COMMANDS LIST WHEN SPLITTING SPACE IS REACHED
-void ft_empty_buffer(char *s, t_list **command)
+
+void		ft_empty_buffer(char *s, t_list **command)
 {
 	t_list	*tmp;
 	char	*copy;
@@ -275,14 +276,9 @@ void ft_empty_buffer(char *s, t_list **command)
 	copy = ft_strdup(s);
 	tmp = ft_lstnew(copy);
 	ft_lstadd_back(command, tmp);
-	//if (s)
-	//{	
-	//	free(s);
-	//	s = NULL;
-	//}
 }
 
-t_list *ft_parse(char *s)
+t_list		*ft_parse(char *s)
 {
 	t_list	*command;
 	char	*res;
@@ -296,11 +292,11 @@ t_list *ft_parse(char *s)
 	{
 		if (s[i] == '\"')
 		{
-			res = ft_apply_ft(s, &i, &ft_double, res);
+			res = ft_apply(s, &i, &ft_double, res);
 			continue;
 		}
 		else if (s[i] == ' ')
-		{	
+		{
 			ft_empty_buffer(res, &command);
 			free(res);
 			res = NULL;
@@ -308,16 +304,16 @@ t_list *ft_parse(char *s)
 		}
 		if (s[i] == '\'')
 		{
-			res = ft_apply_ft(s, &i, &ft_single, res);
+			res = ft_apply(s, &i, &ft_single, res);
 			continue;
 		}
 		else if (s[i] == '$')
 		{
-			res = ft_search_var(&s[i], &inc, 0);
+			res = ft_search_var(s, &inc, 0, &i);
 			i += inc;
 		}
 		else
-			res = ft_apply_ft(s, &i, &ft_string, res);
+			res = ft_apply(s, &i, &ft_string, res);
 	}
 	if (res)
 		ft_empty_buffer(res, &command);
@@ -328,26 +324,20 @@ t_list *ft_parse(char *s)
 	}
 }
 
-int main()
+int	main(void)
 {
-	// t_list *command;
-	char *s;
-	ft_new_env();
-	s = ft_strdup("\"${VAR}\" \"\'lol\'\" ${VR} mdr");
 	int n = 0;
 	int k = 5;
 	int *j = &k;
 	int *i = &n;
-	
+	char *s;
+
+	ft_new_env();
+/*	
+**	s = ft_strdup("\"${VAR}\" \"\'lol\'\" ${VR} mdr");
+*/
+	s = ft_strdup("\'\"lol&\"\'  \"\'lol\'\" $VAR ${VAR} $WRONG \"$VAR\", \'$VAR\' \\");
 	ft_parse(s);
 	printf("%s\n", s);
-	// command = ft_parse(s);
-	//int i =0;
-
-	//while (res[i])
-	//{
-	//	printf("%s", res[i]);
-	//	i++;
-	//}
-	return 0;
+	return (0);
 }
